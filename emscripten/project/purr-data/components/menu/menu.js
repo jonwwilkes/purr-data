0c9c42e4b67696119cac7311f20927c490ee03e1
Prakhar Agarwal <prakhar@everything.net.in>
2021-04-11 12:53:27 +0530

    added default browser zoom in

--- emscripten/project/purr-data/components/menu/menu.js
+++ emscripten/project/purr-data/components/menu/menu.js
@@ -110,12 +110,6 @@ function menu_options(type, w, cid) {
         },
 
         // View entries
-        "view-zoomin": {
-            label: l("menu.zoomin"),
-            key: pdbundle.shortcuts.menu.zoomin_web.key,
-            modifiers: pdbundle.shortcuts.menu.zoomin_web.modifiers,
-            tooltip: l("menu.zoomin_tt")
-        },
         "view-zoomout": {
             label: l("menu.zoomout"),
             key: pdbundle.shortcuts.menu.zoomout_web.key,
@@ -436,6 +430,12 @@ function menu_options(type, w, cid) {
         },
 
         // View section
+        "view-zoomin": {
+            label: l("menu.zoomin"),
+            key: pdbundle.shortcuts.menu.zoomin_web.key,
+            modifiers: pdbundle.shortcuts.menu.zoomin_web.modifiers,
+            tooltip: l("menu.zoomin_tt")
+        },
         ...view_base,
 
         // View entries
